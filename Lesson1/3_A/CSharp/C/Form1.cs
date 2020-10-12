8375f693694e220b97476ea6d72b3e1529191f2e
CarboneLorenzo <64482423+CarboneLorenzo@users.noreply.github.com>
2020-10-12 15:05:32 +0200

    3_A

---
+++ Lesson1/3_A/CSharp/C/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            richTextBox1.AllowDrop = true;
+            this.richTextBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.richTextBox1_DragDrop);
+            this.richTextBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.richTextBox1_DragEnter);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text = "Pong" + Environment.NewLine;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.ResetText();
+        }
+
+        private void richTextBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.richTextBox1.BackColor = Color.Red;
+        }
+
+        private void richTextBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.richTextBox1.BackColor = Color.White;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void richTextBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                richTextBox1.AppendText(s[i] + Environment.NewLine);
+        }
+    }
+}
---
+++ Lesson1/3_A/VB_Net/VB_Net/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.RichTextBox1.Text = "Pong" + Environment.NewLine
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.RichTextBox1.ResetText()
+    End Sub
+
+    Private Sub RichTextBox1_MouseEnter(sender As Object, e As EventArgs) Handles RichTextBox1.MouseEnter
+        Me.RichTextBox1.BackColor = Color.Red
+    End Sub
+
+    Private Sub RichTextBox1_MouseLeave(sender As Object, e As EventArgs) Handles RichTextBox1.MouseLeave
+        Me.RichTextBox1.BackColor = Color.White
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+        Me.RichTextBox1.AllowDrop = True
+    End Sub
+
+    Private Sub RichTextBox1_DragDrop(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles RichTextBox1.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        For Each path In files
+            Me.RichTextBox1.AppendText(path + Environment.NewLine)
+        Next
+    End Sub
+
+    Private Sub RichTextBox1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles RichTextBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+End Class
