3999a68f504cd25097666ffb7b8223e473dc320d
CarboneLorenzo <carbone.1808892@studenti.uniroma1.it>
2020-11-22 13:42:32 +0100

    lesson7

---
+++ Lesson7/15_A/CSharp/CSharp/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1190, 560);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of observations:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(286, 47);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Max:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(416, 47);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(489, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Min:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(539, 47);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1214, 665);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ Lesson7/15_A/CSharp/CSharp/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+        Bitmap b;
+        Graphics g;
+        Font smallFont = new Font("Calibri", 10, FontStyle.Regular, GraphicsUnit.Pixel);
+        int n = 100;
+        int max = 20;
+        int min = 1;
+
+        private void initializeGraphics()
+        {
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(b);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+        }
+
+        private void representPath()
+        {
+            Rectangle viewport = new Rectangle(20, 20, (pictureBox1.Width-21), (pictureBox1.Height / 3));
+            Rectangle viewport2 = new Rectangle(20, (pictureBox1.Height / 3)+60, (pictureBox1.Width - 21), (pictureBox1.Height / 3));
+            //g.DrawRectangle(Pens.Black, viewport);
+            double maxX_Window = max;
+            double maxY_Window = 1;
+            double minX_Window = min;
+            double minY_Window = 0;
+            double RangeX = maxX_Window - minX_Window;
+            double RangeY = maxY_Window - minY_Window;
+
+            int x_Device = X_viewPort(maxX_Window, viewport, minX_Window, RangeX);
+            int y_Device = Y_viewPort(maxY_Window, viewport, minY_Window, RangeY);
+            int x_zero = X_viewPort(minX_Window, viewport, minX_Window, RangeX);
+            int y_zero = Y_viewPort(minY_Window, viewport, minY_Window, RangeY);
+
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_zero, y_Device), new Point(x_Device, y_Device));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_zero, y_zero), new Point(x_Device, y_zero));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_Device, y_Device), new Point(x_Device, y_zero));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_zero, y_zero), new Point(x_zero, y_Device));
+
+            int x_Device2 = X_viewPort(maxX_Window, viewport2, minX_Window, RangeX);
+            int y_Device2 = Y_viewPort(maxY_Window, viewport2, minY_Window, RangeY);
+            int x_zero2 = X_viewPort(minX_Window, viewport2, minX_Window, RangeX);
+            int y_zero2 = Y_viewPort(minY_Window, viewport2, minY_Window, RangeY);
+
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_zero2, y_Device2), new Point(x_Device2, y_Device2));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_zero2, y_zero2), new Point(x_Device2, y_zero2));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_Device2, y_Device2), new Point(x_Device2, y_zero2));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_zero2, y_zero2), new Point(x_zero2, y_Device2));
+
+            for (double i=0; i<=10; i++)
+            {
+                double point = i / 10.0;
+                g.DrawString(point.ToString(), smallFont, Brushes.Black, new Point(x_zero-20, Y_viewPort(point, viewport, minY_Window, RangeY)));
+                g.DrawString(point.ToString(), smallFont, Brushes.Black, new Point(x_zero2 - 20, Y_viewPort(point, viewport2, minY_Window, RangeY)));
+            }
+
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            for (int i=min; i<max; i++)
+            {
+                freq.Add(i, 0);
+                g.DrawLine(new Pen(Brushes.LightGray), new Point(X_viewPort(i, viewport, minX_Window, RangeX), y_zero), new Point(X_viewPort(i, viewport, minX_Window, RangeX), y_Device));
+                g.DrawString(i.ToString(), smallFont, Brushes.Black, new Point(X_viewPort(i, viewport, minX_Window, RangeX), y_zero));
+
+                g.DrawLine(new Pen(Brushes.LightGray), new Point(X_viewPort(i, viewport2, minX_Window, RangeX), y_zero2), new Point(X_viewPort(i, viewport2, minX_Window, RangeX), y_Device2));
+                g.DrawString(i.ToString(), smallFont, Brushes.Black, new Point(X_viewPort(i, viewport2, minX_Window, RangeX), y_zero2));
+
+            }
+
+            List<int> keys = new List<int>(freq.Keys);
+            for(int j=0; j<n; j++)
+            {
+                int obs = r.Next(min, max);
+                foreach(int k in keys)
+                {
+                    if(k == obs)
+                    {
+                        freq[k]++;
+                    }
+                }
+            }
+
+            int count = 0;
+            Point start = new Point(X_viewPort(minX_Window, viewport, minX_Window, RangeX), Y_viewPort(minY_Window, viewport, minY_Window, RangeY));
+            foreach(KeyValuePair<int,int> kv in freq)
+            {
+                g.DrawLine(new Pen(Brushes.Blue), start, new Point(X_viewPort(kv.Key, viewport, minX_Window, RangeX), start.Y));
+                start = new Point(X_viewPort(kv.Key, viewport, minX_Window, RangeX), start.Y);
+                if (kv.Value>0)
+                {
+                    count += kv.Value;
+                    g.DrawLine(new Pen(Brushes.Blue), start, new Point(start.X, Y_viewPort((double)count/(double)n, viewport, minY_Window, RangeY)));
+                    start = new Point(start.X, Y_viewPort((double)count / (double)n, viewport, minY_Window, RangeY));
+                }
+
+                int rectx = X_viewPort(kv.Key, viewport2, minX_Window, RangeX);
+                int recty = Y_viewPort(((double)kv.Value / (double)n), viewport2, minY_Window, RangeY);
+                Rectangle rect = new Rectangle(rectx, recty, X_viewPort(kv.Key+1, viewport2, minX_Window, RangeX) - rectx, y_zero2 - recty);
+                g.FillRectangle(Brushes.Blue, rect);
+                g.DrawRectangle(Pens.Red, rect);
+
+            }
+
+
+            pictureBox1.Image = b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (max>min)
+            {
+                initializeGraphics();
+                representPath();
+            }
+            else
+            {
+                MessageBox.Show("max must be bigger than min!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        int X_viewPort(double x_World, Rectangle viewPort, double minX, double rangeX)
+        {
+            return (int)(viewPort.Left + ((viewPort.Width * (x_World - minX)) / rangeX));
+        }
+
+        int Y_viewPort(double y_World, Rectangle viewPort, double minY, double rangeY)
+        {
+            return (int)(viewPort.Top + viewPort.Height - ((viewPort.Height * (y_World - minY)) / rangeY));
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            n = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            min = (int)numericUpDown3.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            max = (int)numericUpDown2.Value;
+        }
+    }
+}
