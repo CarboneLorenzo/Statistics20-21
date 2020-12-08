364c038e7659d09667d4317494714d0f1ebbe5d2
CarboneLorenzo <carbone.1808892@studenti.uniroma1.it>
2020-12-08 17:10:04 +0100

    Lesson9

---
+++ Lesson9/20_A/CSharp/CSharp/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1090, 551);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(270, 30);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of paths:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(463, 28);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of extractions: ";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(270, 61);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Variance: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(463, 61);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown5.TabIndex = 11;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "J position:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(516, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Histogram";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 650);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lesson9/20_A/CSharp/CSharp/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        Bitmap b;
+        Graphics g;
+        Font smallFont = new Font("Calibri", 20, FontStyle.Regular, GraphicsUnit.Pixel);
+        Font otherFont = new Font("Calibri", 10, FontStyle.Regular, GraphicsUnit.Pixel);
+        double variance = 50;
+        int m = 10;
+        int n = 200;
+        int splitter = 100;
+
+        private void initializeGraphics()
+        {
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(b);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+        }
+
+        private void representPath()
+        {
+            
+            Rectangle viewport = new Rectangle(20,(pictureBox1.Height/4), (pictureBox1.Width - 10) , ( pictureBox1.Height/2));
+            g.DrawLine(new Pen(Brushes.Black), new Point(viewport.X, viewport.Y+viewport.Height), new Point(viewport.X, viewport.Y));
+            //g.DrawRectangle(Pens.Black, viewport);
+            double maxX_Window = n+(n/4);
+            double maxY_Window = n;
+            double minX_Window = 0;
+            double minY_Window = -n;
+            double RangeX = maxX_Window - minX_Window;
+            double RangeY = maxY_Window - minY_Window;
+
+            int x_Device = X_viewPort(maxX_Window, viewport, minX_Window, RangeX);
+            int y_Device = Y_viewPort(maxY_Window, viewport, minY_Window, RangeY);
+            int x_zero = X_viewPort(minX_Window, viewport, minX_Window, RangeX);
+            int y_zero = Y_viewPort(minY_Window, viewport, minY_Window, RangeY);
+            int x_real_zero = X_viewPort(0, viewport, minX_Window, RangeX);
+            int y_real_zero = Y_viewPort(0, viewport, minY_Window, RangeY);
+
+            int x_Device_less = X_viewPort(maxX_Window-(n/4), viewport, minX_Window, RangeX);
+            int x_splitter = X_viewPort(splitter, viewport, minX_Window, RangeX);
+            
+
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_Device_less, y_Device), new Point(x_Device_less, y_zero));
+            g.DrawString("N", smallFont, Brushes.Black, new Point(x_Device_less, y_Device-20));
+            g.DrawLine(new Pen(Brushes.Black), new Point(x_splitter, y_Device), new Point(x_splitter, y_zero));
+            g.DrawString("J", smallFont, Brushes.Black, new Point(x_splitter, y_Device-20));
+            g.DrawString(n.ToString(), otherFont, Brushes.Black, new Point(x_zero - 20, y_Device));
+            g.DrawString("-" + n.ToString(), otherFont, Brushes.Black, new Point(x_zero - 20, y_zero - 3));
+            g.DrawString("0", otherFont, Brushes.Black, new Point(x_real_zero - 10, y_real_zero - 10));
+
+            for (int i=0; i<n; i+=10)
+            {
+                int x_index = X_viewPort( i, viewport, minX_Window, RangeX);
+                g.DrawString(i.ToString(), otherFont, Brushes.Black, new Point(x_index, y_zero));
+
+            }
+            
+
+
+
+
+            List<double> listj = new List<double>();
+            List<double> listn = new List<double>();
+            for (int i = 1; i<=m; i++)
+            {
+                double previousStep = 0;
+                Color randomColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+                Brush brush = new SolidBrush(randomColor);
+                for (int j = 1; j<=n; j++)
+                {
+                    Point start = new Point(X_viewPort(j-1, viewport, minX_Window, RangeX), Y_viewPort(previousStep, viewport, minY_Window, RangeY));
+
+                    double value = NextStandardGaussianDouble(r);
+                    double randomStep = (float)(variance * Math.Sqrt(((double)1 / (double)n)) * value++);
+                    double step = previousStep + randomStep;
+                    previousStep = (float)step;
+                    
+
+                    if (j>1)
+                    {        
+                        g.DrawLine(new Pen(brush), start, new Point(X_viewPort(j, viewport, minX_Window, RangeX), Y_viewPort(step, viewport, minY_Window, RangeY)));
+                    }
+                    else
+                    {
+                        g.DrawLine(new Pen(brush), new Point(X_viewPort(0, viewport, minX_Window, RangeX), Y_viewPort(0, viewport, minY_Window, RangeY)), 
+                                            new Point(X_viewPort(j, viewport, minX_Window, RangeX), Y_viewPort(step, viewport, minY_Window, RangeY)));
+                    }
+
+                    if(j==splitter)
+                    {
+                        listj.Add(step);
+                    }
+
+                    if (j == n)
+                    {
+                        listn.Add(step);
+                    }
+
+                }
+
+                if(checkBox1.Checked)
+                {
+                    Dictionary<int, int> histoj = new Dictionary<int, int>();
+                    for(int x=-n; x<n; x+=5)
+                    {
+                        histoj.Add(x, 0);
+                    }
+
+                    List<int> keysj = new List<int>(histoj.Keys);
+                    foreach (double z in listj)
+                    {
+                        int x = (int)z;
+                        foreach(int y in keysj)
+                        {
+                           
+                            if(x>=y && x < (y + 5))
+                            {
+                                histoj[y]+=1;
+                            }
+                        }
+                    }
+
+                    foreach (KeyValuePair<int, int> y in histoj)
+                    {
+                        double par = (double)y.Key;
+                        int y_height = Y_viewPort((par + 5), viewport, minY_Window, RangeY);
+                        int y_freq = Y_viewPort(par, viewport, minY_Window, RangeY);
+                        int x_width = X_viewPort(y.Value, viewport, minX_Window, RangeX);
+
+
+                        Rectangle rect = new Rectangle(x_splitter, y_height, (int)y.Value * 10, y_freq - y_height);
+                        
+                        g.FillRectangle(Brushes.Blue, rect);
+                        g.DrawRectangle(Pens.Red, rect);
+                    }
+
+                    Dictionary<int, int> histon = new Dictionary<int, int>();
+                    for (int x = -n; x < n; x+=5)
+                    {
+                        histon.Add(x, 0);
+                    }
+
+                    List<int> keysn = new List<int>(histon.Keys);
+                    foreach (double z in listn)
+                    {
+                        int x = (int)z;
+                        foreach (int y in keysn)
+                        {
+          
+                            if (x >= y && x < (y + 5))
+                            {
+                                histon[y] += 1;
+                            }
+                        }
+                    }
+
+                    foreach (KeyValuePair<int, int> y in histon)
+                    {
+                        double par = (double)y.Key;
+                        int y_height = Y_viewPort((par + 5), viewport, minY_Window, RangeY);
+                        int y_freq = Y_viewPort(par, viewport, minY_Window, RangeY);
+                        int x_width = X_viewPort(y.Value, viewport, minX_Window, RangeX);
+
+
+                        Rectangle rect = new Rectangle(x_Device_less, y_height, y.Value * 10, y_freq - y_height);
+                        g.FillRectangle(Brushes.Blue, rect);
+                        g.DrawRectangle(Pens.Red, rect);
+                    }
+
+                }
+            }
+
+            pictureBox1.Image = b;
+        }
+
+        private double NextStandardGaussianDouble(Random r)
+        {
+            double u, v, S;
+            do
+            {
+                u = 2.0 * r.NextDouble() - 1.0;
+                v = 2.0 * r.NextDouble() - 1.0;
+                S = u * u + v * v;
+            }
+            while (S >= 1.0);
+
+            double fac = Math.Sqrt(-2.0 * Math.Log(S) / S);
+            return u * fac;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(splitter<n)
+            {
+                initializeGraphics();
+                representPath();
+            }
+            else
+            {
+                MessageBox.Show("J must be smaller than N!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        int X_viewPort(double x_World, Rectangle viewPort, double minX, double rangeX)
+        {
+            return (int)(viewPort.Left + ((viewPort.Width * (x_World - minX)) / rangeX));
+        }
+
+        int Y_viewPort(double y_World, Rectangle viewPort, double minY, double rangeY)
+        {
+            return (int)(viewPort.Top + viewPort.Height - ((viewPort.Height * (y_World - minY)) / rangeY));
+        }
+
+        private double func_y(int p, int x)
+        {
+            return (double) ( p ^ (x* ((1 - p) ^ (1 - x)) ));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            m = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            n = (int)numericUpDown2.Value;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            variance = (double)numericUpDown3.Value;
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+            splitter = (int)numericUpDown5.Value;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
